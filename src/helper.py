b2f47b2a8e31ffc222dfc72a23d2b25b737f8c20
ZP.Yin <ZP.Yin@whu.edu.cn>
2020-02-06 16:10:39 +0800

    add analysis func

---
+++ src/helper.py
@@ -0,0 +1,22 @@
+
+
+def dailyMean(time, counts):
+    """
+    calculate the daily mean.
+
+    Parameters
+    ----------
+    time: datetime object
+        timestamp for each record.
+    counts: numpy numeric
+        data record.
+
+    Returns
+    -------
+    dailyTime: datetime object
+        timestamp for each day.
+    dailyCount: numpy numeric
+        data record for each day.
+    """
+
+    pass
